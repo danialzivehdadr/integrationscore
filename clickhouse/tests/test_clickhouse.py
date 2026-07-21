19e1098f10d8208ae0d0f372992d5e547dd7194d
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-21 23:21:47 +0000

    Initialize revert-23206-aarakke/ddev-python-313-target

---
+++ clickhouse/tests/test_clickhouse.py
@@ -0,0 +1,102 @@
+# (C) Datadog, Inc. 2019-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
+
+from datadog_checks.clickhouse import ClickhouseCheck
+from datadog_checks.dev.utils import get_metadata_metrics
+
+from .common import CLICKHOUSE_VERSION
+from .metrics import OPTIONAL_METRICS, get_metrics
+
+pytestmark = [pytest.mark.integration, pytest.mark.usefixtures('dd_environment')]
+
+
+def test_check(aggregator, instance, dd_run_check):
+    check = ClickhouseCheck('clickhouse', {}, [instance])
+    dd_run_check(check)
+    server_tag = 'server:{}'.format(instance['server'])
+    port_tag = 'port:{}'.format(instance['port'])
+    db_hostname_tag = 'database_hostname:{}'.format(instance['server'])
+    db_instance_tag = 'database_instance:{}:{}:default'.format(instance['server'], instance['port'])
+    metrics = get_metrics(CLICKHOUSE_VERSION)
+
+    for metric in metrics:
+        aggregator.assert_metric_has_tag(metric, port_tag, at_least=1)
+        aggregator.assert_metric_has_tag(metric, server_tag, at_least=1)
+        aggregator.assert_metric_has_tag(metric, 'db:default', at_least=1)
+        aggregator.assert_metric_has_tag(metric, 'foo:bar', at_least=1)
+
+    aggregator.assert_metric(
+        'clickhouse.dictionary.item.current',
+        tags=[server_tag, port_tag, 'db:default', 'foo:bar', 'dictionary:test', db_hostname_tag, db_instance_tag],
+        at_least=1,
+    )
+
+    for metric in OPTIONAL_METRICS:
+        aggregator.assert_metric(metric, at_least=0)
+
+    aggregator.assert_service_check("clickhouse.can_connect", count=1)
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+
+
+def test_custom_queries(aggregator, instance, dd_run_check):
+    instance['custom_queries'] = [
+        {
+            'tags': ['test:clickhouse'],
+            'query': 'SELECT COUNT(*) FROM system.settings WHERE changed',
+            'columns': [{'name': 'settings.changed', 'type': 'gauge'}],
+        }
+    ]
+
+    check = ClickhouseCheck('clickhouse', {}, [instance])
+    dd_run_check(check)
+
+    aggregator.assert_metric(
+        'clickhouse.settings.changed',
+        metric_type=0,
+        tags=[
+            'server:{}'.format(instance['server']),
+            'port:{}'.format(instance['port']),
+            'db:default',
+            'foo:bar',
+            'test:clickhouse',
+            'database_hostname:{}'.format(instance['server']),
+            'database_instance:{}:{}:default'.format(instance['server'], instance['port']),
+        ],
+    )
+
+
+@pytest.mark.skipif(CLICKHOUSE_VERSION == 'latest', reason='Version `latest` is ever-changing, skipping')
+def test_version_metadata(instance, datadog_agent, dd_run_check):
+    check = ClickhouseCheck('clickhouse', {}, [instance])
+    check.check_id = 'test:123'
+    dd_run_check(check)
+
+    datadog_agent.assert_metadata(
+        'test:123', {'version.scheme': 'calver', 'version.year': CLICKHOUSE_VERSION.split(".")[0]}
+    )
+
+
+def test_database_instance_metadata(aggregator, instance, datadog_agent, dd_run_check):
+    """Test that database_instance metadata is sent correctly."""
+    check = ClickhouseCheck('clickhouse', {}, [instance])
+    check.check_id = 'test:456'
+    dd_run_check(check)
+
+    # Get database monitoring metadata events
+    dbm_metadata = aggregator.get_event_platform_events("dbm-metadata")
+
+    # Find the database_instance event
+    event = next((e for e in dbm_metadata if e['kind'] == 'database_instance'), None)
+
+    assert event is not None, "database_instance metadata event should be sent"
+    assert event['dbms'] == 'clickhouse'
+    assert event['kind'] == 'database_instance'
+    assert event['database_instance'] == check.database_identifier
+    assert event['collection_interval'] == 300
+    assert 'metadata' in event
+    assert 'dbm' in event['metadata']
+    assert 'connection_host' in event['metadata']
+    assert event['metadata']['connection_host'] == instance['server']
