c0b1b372883ec6739380c86fde3b33ce505d2447
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-21 22:56:55 +0000

    Initialize sangeeta.shivajirao/self-hosted-single-node-connection

---
+++ haproxy/tests/test_check.py
@@ -0,0 +1,26 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
+
+from .common import requires_new_environment
+
+pytestmark = [requires_new_environment, pytest.mark.usefixtures('dd_environment')]
+
+
+def test_check(aggregator, dd_run_check, instancev1, check, prometheus_metrics):
+    dd_run_check(check(instancev1))
+
+    for metric in prometheus_metrics:
+        aggregator.assert_metric('haproxy.{}'.format(metric))
+
+    aggregator.assert_all_metrics_covered()
+
+
+def test_checkv2(aggregator, dd_run_check, check, instancev2, prometheus_metricsv2):
+    dd_run_check(check(instancev2))
+
+    for metric in prometheus_metricsv2:
+        aggregator.assert_metric('haproxy.{}'.format(metric))
+
+    aggregator.assert_all_metrics_covered()
