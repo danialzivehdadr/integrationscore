950ee29958f5c13e941e1f83092afff3598ad0c4
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-22 00:42:18 +0330

    Initialize 7.60.x

---
+++ ntp/README.md
@@ -0,0 +1,64 @@
+# NTP check
+
+## Overview
+
+The Network Time Protocol (NTP) integration is enabled by default and reports the time offset from an ntp server every 15 minutes. When the local Agent's time is more than 15 seconds off from the Datadog service and other hosts you are monitoring, you may experience:
+
+- Incorrect alert triggers
+- Metric delays
+- Gaps in graphs of metrics
+
+By default, the check detects which cloud provider the Agent is running on and uses the private
+NTP server of that cloud provider, if available. If no cloud provider is detected, the agent will
+default to the NTP servers below:
+
+- `0.datadog.pool.ntp.org`
+- `1.datadog.pool.ntp.org`
+- `2.datadog.pool.ntp.org`
+- `3.datadog.pool.ntp.org`
+
+**Note:** NTP requests do not support proxy settings.
+
+## Setup
+
+### Installation
+
+The NTP check is included in the [Datadog Agent][1] package, so you don't need to install anything else on your servers.
+
+### Configuration
+
+The Agent enables the NTP check by default. To configure the check yourself, edit the file `ntp.d/conf.yaml` in the `conf.d/` folder at the root of your [Agent's configuration directory][2]. See the [sample ntp.d/conf.yaml][3] for all available configuration options.
+
+Outgoing UDP traffic over the port `123` should be allowed so the Agent can confirm that the local server time is reasonably accurate according to the Datadog NTP servers.
+
+**Note**: If you edit the Datadog-NTP check configuration file, [restart the Agent][4] to effect any configuration changes.
+
+### Validation
+
+[Run the Agent's `status` subcommand][5] and look for `ntp` under the Checks section.
+
+## Data Collected
+
+### Metrics
+
+See [metadata.csv][6] for a list of metrics provided by this check.
+
+### Events
+
+The NTP check does not include any events.
+
+### Service Checks
+
+See [service_checks.json][7] for a list of service checks provided by this integration.
+
+## Troubleshooting
+Need help? Contact [Datadog support][8].
+
+[1]: https://app.datadoghq.com/account/settings/agent/latest
+[2]: https://docs.datadoghq.com/agent/guide/agent-configuration-files/#agent-configuration-directory
+[3]: https://github.com/DataDog/datadog-agent/blob/master/cmd/agent/dist/conf.d/ntp.d/conf.yaml.default
+[4]: https://docs.datadoghq.com/agent/guide/agent-commands/#start-stop-and-restart-the-agent
+[5]: https://docs.datadoghq.com/agent/guide/agent-commands/#agent-status-and-information
+[6]: https://github.com/DataDog/integrations-core/blob/master/ntp/metadata.csv
+[7]: https://github.com/DataDog/integrations-core/blob/master/ntp/assets/service_checks.json
+[8]: https://docs.datadoghq.com/help/
---
+++ redisdb/tests/test_unit.py
@@ -0,0 +1,104 @@
+# (C) Datadog, Inc. 2018-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import mock
+import pytest
+
+from datadog_checks.dev.utils import get_metadata_metrics
+
+pytestmark = pytest.mark.unit
+
+
+def test_init(check, redis_instance):
+    check = check(redis_instance)
+    assert check.connections == {}
+    assert check.last_timestamp_seen == 0
+
+
+def test__get_conn(check, redis_instance):
+    check = check(redis_instance)
+    instance = {}
+
+    # create a connection
+    check._get_conn(instance)
+    key1, conn1 = next(iter(check.connections.items()))
+
+    # assert connection is cached
+    check._get_conn(instance)
+    key2, conn2 = next(iter(check.connections.items()))
+    assert key2 == key1
+    assert conn2 == conn1
+
+    # disable cache and assert connection has changed
+    instance['disable_connection_cache'] = True
+    check._get_conn(instance)
+    key2, conn2 = next(iter(check.connections.items()))
+    assert key2 == key1
+    assert conn2 != conn1
+
+
+@pytest.mark.parametrize(
+    'info, expected_calls_value, expected_usec_per_call_value, expected_tags',
+    [
+        pytest.param(
+            {'cmdstat_lpush': {'usec_per_call': 14.00, 'usec': 56, 'calls': 4}},
+            4,
+            14,
+            ['command:lpush', 'foo:bar'],
+            id='lpush',
+        ),
+        pytest.param(
+            # this is from a real use case in Redis >5.0 where this line can be
+            # seen (notice the double ':')
+            # cmdstat_host::calls=2,usec=145,usec_per_call=72.50
+            {'cmdstat_host': {'usec_per_call': 72.5, 'usec': 145, ':calls': 2}},
+            2,
+            72.5,
+            ['foo:bar', 'command:host'],
+            id="cmdstat_host with double ':'",
+        ),
+    ],
+)
+def test__check_command_stats_host(
+    check, aggregator, redis_instance, info, expected_calls_value, expected_usec_per_call_value, expected_tags
+):
+    check = check(redis_instance)
+    conn = mock.MagicMock()
+    conn.info.return_value = info
+    check._check_command_stats(conn, ['foo:bar'])
+
+    aggregator.assert_metric('redis.command.calls', value=expected_calls_value, count=1, tags=expected_tags)
+    aggregator.assert_metric(
+        'redis.command.usec_per_call', value=expected_usec_per_call_value, count=1, tags=expected_tags
+    )
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+
+
+def test__check_total_commands_processed_not_present(check, aggregator, redis_instance):
+    """
+    The check shouldn't send the `redis.net.commands` metric if `total_commands_processed` is not present in `c.info`
+    """
+    redis_check = check(redis_instance)
+    conn = mock.MagicMock()
+    conn.info.return_value = {}
+
+    # Run the check
+    redis_check._check_total_commands_processed(conn.info(), [])
+
+    # Assert that no metrics were sent
+    aggregator.assert_metric('redis.net.commands', count=0)
+
+
+def test__check_total_commands_processed_present(check, aggregator, redis_instance):
+    """
+    The check should send the `redis.net.commands` metric if `total_commands_processed` is present in `c.info`
+    """
+    redis_check = check(redis_instance)
+    conn = mock.MagicMock()
+    conn.info.return_value = {'total_commands_processed': 1000}
+
+    # Run the check
+    redis_check._check_total_commands_processed(conn.info(), ['test_total_commands_processed'])
+
+    # Assert that the `redis.net.commands` metric was sent
+    aggregator.assert_metric('redis.net.commands', value=1000, tags=['test_total_commands_processed'])
