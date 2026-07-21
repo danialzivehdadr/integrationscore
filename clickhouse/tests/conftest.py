ac83a815302576c397d56e79bb26c3d48844d287
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-21 23:03:15 +0000

    Initialize noueman/test-ssh

---
+++ clickhouse/tests/conftest.py
@@ -0,0 +1,57 @@
+# (C) Datadog, Inc. 2019-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from copy import deepcopy
+
+import clickhouse_connect
+import pytest
+
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import CheckDockerLogs, CheckEndpoints, WaitFor
+
+from . import common
+
+
+@pytest.fixture(scope='session')
+def dd_environment():
+    conditions = []
+
+    for i in range(6):
+        conditions.append(CheckEndpoints(['http://{}:{}'.format(common.HOST, common.HTTP_START_PORT + i)]))
+        conditions.append(
+            CheckDockerLogs(
+                'clickhouse-0{}'.format(i + 1), 'Logging errors to /var/log/clickhouse-server/clickhouse-server.err.log'
+            )
+        )
+
+    conditions.append(
+        WaitFor(
+            ping_clickhouse(
+                common.CONFIG['server'],
+                common.CONFIG['port'],
+                common.CONFIG['username'],
+                common.CONFIG['password'],
+            )
+        )
+    )
+
+    with docker_run(common.COMPOSE_FILE, conditions=conditions, sleep=10, attempts=2):
+        yield common.CONFIG
+
+
+@pytest.fixture
+def instance():
+    return deepcopy(common.CONFIG)
+
+
+def ping_clickhouse(host, port, username, password):
+    def _ping_clickhouse():
+        client = clickhouse_connect.get_client(
+            host=host,
+            port=port,
+            username=username,
+            password=password,
+        )
+        return client.ping()
+
+    return _ping_clickhouse
