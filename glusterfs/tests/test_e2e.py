5dd2cabe99c4f459f1a42eb017811631b1b203c0
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-21 23:04:11 +0000

    Initialize 7.77.x

---
+++ glusterfs/tests/test_e2e.py
@@ -0,0 +1,21 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import pytest
+
+from datadog_checks.glusterfs import GlusterfsCheck
+
+from .common import EXPECTED_METRICS
+
+pytestmark = pytest.mark.e2e
+
+
+def test_e2e(dd_agent_check, config):
+    aggregator = dd_agent_check(config)
+
+    for metric in EXPECTED_METRICS:
+        aggregator.assert_metric(metric)
+
+    aggregator.assert_service_check("glusterfs.cluster.health", GlusterfsCheck.OK)
+    aggregator.assert_all_metrics_covered()
