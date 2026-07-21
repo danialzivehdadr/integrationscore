9d43cef83533f0a4d52e68c82aed81c8e78c06a7
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-22 00:45:14 +0330

    Initialize 7.68.x

---
+++ ddev/tests/conftest.py
@@ -0,0 +1,258 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from __future__ import annotations
+
+import random
+from contextlib import ExitStack
+from typing import Generator
+
+import pytest
+import vcr
+from datadog_checks.dev.tooling.utils import set_root
+
+from ddev.cli.terminal import Terminal
+from ddev.config.constants import AppEnvVars, ConfigEnvVars
+from ddev.config.file import DDEV_TOML, ConfigFileWithOverrides
+from ddev.e2e.constants import E2EEnvVars
+from ddev.repo.core import Repository
+from ddev.utils.ci import running_in_ci
+from ddev.utils.fs import Path, temp_directory
+from ddev.utils.github import GitHubManager
+from ddev.utils.platform import Platform
+
+from .helpers import PLATFORM
+from .helpers.git import ClonedRepo
+from .helpers.runner import CliRunner
+
+
+@pytest.fixture(scope='session')
+def ddev():
+    from ddev import cli
+
+    return CliRunner(cli.ddev)
+
+
+@pytest.fixture(scope='session')
+def platform() -> Platform:
+    return PLATFORM
+
+
+@pytest.fixture(scope='session')
+def docker_path(platform):
+    return platform.format_for_subprocess(['docker'], shell=False)[0]
+
+
+@pytest.fixture(scope='session')
+def terminal() -> Terminal:
+    return Terminal(verbosity=0, enable_color=False, interactive=False)
+
+
+@pytest.fixture(scope='session')
+def local_repo() -> Path:
+    return Path(__file__).resolve().parent.parent.parent
+
+
+@pytest.fixture(scope='session')
+def valid_integrations(local_repo) -> list[str]:
+    repo = Repository(local_repo.name, str(local_repo))
+    return [path.name for path in repo.integrations.iter_all(['all'])]
+
+
+@pytest.fixture
+def github_manager(local_repo, config_file, terminal) -> GitHubManager:
+    return GitHubManager(
+        Repository(local_repo.name, str(local_repo)),
+        user=config_file.model.github.user,
+        token=config_file.model.github.token,
+        status=terminal.status,
+    )
+
+
+@pytest.fixture
+def valid_integration(valid_integrations) -> str:
+    return random.choice(valid_integrations)
+
+
+@pytest.fixture(autouse=True)
+def config_file(tmp_path, monkeypatch, local_repo, mocker) -> ConfigFileWithOverrides:
+    for env_var in (
+        'FORCE_COLOR',
+        'DD_ENV',
+        'DD_SERVICE',
+        'DD_SITE',
+        'DD_LOGS_CONFIG_LOGS_DD_URL',
+        'DD_DD_URL',
+        'DD_API_KEY',
+        'DD_APP_KEY',
+        'DDEV_REPO',
+        'DDEV_TEST_ENABLE_TRACING',
+        'PYTHON_FILTER',
+        E2EEnvVars.AGENT_BUILD,
+        E2EEnvVars.AGENT_BUILD_PY2,
+        'HATCH_VERBOSE',
+        'HATCH_QUIET',
+    ):
+        monkeypatch.delenv(env_var, raising=False)
+
+    path = Path(tmp_path, 'config.toml')
+    monkeypatch.setenv(ConfigEnvVars.CONFIG, str(path))
+
+    config = ConfigFileWithOverrides(path)
+    config.reset()
+
+    # Provide a real default for times when tests have no need to modify the repo
+    config.global_model.repos['core'] = str(local_repo)
+    config.save()
+
+    return config
+
+
+@pytest.fixture
+def overrides_config(temp_dir) -> Generator[Path]:
+    """Creates a temporary overrides config file in the temp current directory."""
+    with temp_dir.as_cwd():
+        ddev_toml = temp_dir / DDEV_TOML
+        ddev_toml.touch()
+        yield ddev_toml
+
+
+@pytest.fixture
+def temp_dir(tmp_path) -> Path:
+    path = Path(tmp_path, 'temp')
+    path.mkdir()
+    return path
+
+
+@pytest.fixture(scope='session', autouse=True)
+def isolation() -> Generator[Path, None, None]:
+    with temp_directory() as d:
+        data_dir = d / 'data'
+        data_dir.mkdir()
+
+        default_env_vars = {
+            'DDEV_SELF_TESTING': 'true',
+            ConfigEnvVars.DATA: str(data_dir),
+            AppEnvVars.NO_COLOR: '1',
+            'COLUMNS': '80',
+            'LINES': '24',
+        }
+        with d.as_cwd(default_env_vars):
+            yield d
+
+
+@pytest.fixture(scope='session')
+def local_clone(isolation, local_repo) -> Generator[ClonedRepo, None, None]:
+    cloned_repo_path = isolation / local_repo.name
+
+    # Get the current origin remote url
+    with local_repo.as_cwd():
+        origin_url = PLATFORM.check_command_output(['git', 'remote', 'get-url', 'origin']).strip()
+
+    PLATFORM.check_command_output(
+        ['git', 'clone', '--local', '--shared', '--no-tags', str(local_repo), str(cloned_repo_path)]
+    )
+    with cloned_repo_path.as_cwd():
+        PLATFORM.check_command_output(['git', 'config', 'user.name', 'Foo Bar'])
+        PLATFORM.check_command_output(['git', 'config', 'user.email', 'foo@bar.baz'])
+        PLATFORM.check_command_output(['git', 'config', 'commit.gpgsign', 'false'])
+        PLATFORM.check_command_output(['git', 'config', 'tag.gpgsign', 'false'])
+
+        # Set url to point to the origin of the local source and not to the local repo
+        PLATFORM.check_command_output(['git', 'remote', 'set-url', 'origin', origin_url])
+        # Now fetch latest updates
+        PLATFORM.check_command_output(['git', 'fetch', 'origin'])
+
+    cloned_repo = ClonedRepo(cloned_repo_path, 'origin/master', 'ddev-testing')
+    cloned_repo.reset_branch()
+
+    yield cloned_repo
+
+
+@pytest.fixture
+def repository(local_clone, config_file) -> Generator[ClonedRepo, None, None]:
+    config_file.model.repos['core'] = str(local_clone.path)
+    config_file.save()
+
+    try:
+        yield local_clone
+    finally:
+        set_root('')
+        local_clone.reset_branch()
+
+
+@pytest.fixture
+def repository_as_cwd(repository: ClonedRepo) -> Generator[ClonedRepo, None, None]:
+    with repository.path.as_cwd():
+        yield repository
+
+
+@pytest.fixture(scope='session')
+def default_hostname():
+    import socket
+
+    return socket.gethostname().lower()
+
+
+@pytest.fixture
+def network_replay(local_repo):
+    """
+    To use, run once without record_mode='none' as an argument and then add it in for subsequent runs.
+    """
+    stack = ExitStack()
+
+    def add_cassette(relative_path, *args, **kwargs):
+        # https://vcrpy.readthedocs.io/en/latest/advanced.html#filter-sensitive-data-from-the-request
+        for option, known_values in (
+            ('filter_headers', ['authorization', 'dd-api-key', 'dd-application-key']),
+            ('filter_query_parameters', ['api_key', 'app_key', 'application_key']),
+            ('filter_post_data_parameters', ['api_key', 'app_key']),
+        ):
+            defined_values = list(kwargs.setdefault(option, []))
+            defined_values.extend(known_values)
+            kwargs[option] = defined_values
+
+        cassette = vcr.use_cassette(
+            str(local_repo / "ddev" / "tests" / "fixtures" / "network" / relative_path), *args, **kwargs
+        )
+        stack.enter_context(cassette)
+        return cassette
+
+    with stack:
+        yield add_cassette
+
+
+@pytest.fixture(scope='session')
+def helpers():
+    # https://docs.pytest.org/en/latest/writing_plugins.html#assertion-rewriting
+    pytest.register_assert_rewrite('tests.helpers.api')
+
+    from .helpers import api
+
+    return api
+
+
+def pytest_runtest_setup(item):
+    for marker in item.iter_markers():
+        if marker.name == 'requires_ci' and not running_in_ci():  # no cov
+            pytest.skip('Only running in CI')
+
+        if marker.name == 'requires_windows' and not PLATFORM.windows:
+            pytest.skip('Only running on Windows')
+
+        if marker.name == 'requires_macos' and not PLATFORM.macos:
+            pytest.skip('Only running on macOS')
+
+        if marker.name == 'requires_linux' and not PLATFORM.linux:
+            pytest.skip('Only running on Linux')
+
+        if marker.name == 'requires_unix' and PLATFORM.windows:
+            pytest.skip('Only running on a Linux-based platform')
+
+
+def pytest_configure(config):
+    config.addinivalue_line('markers', 'requires_windows: Tests intended for Windows operating systems')
+    config.addinivalue_line('markers', 'requires_macos: Tests intended for macOS operating systems')
+    config.addinivalue_line('markers', 'requires_linux: Tests intended for Linux operating systems')
+    config.addinivalue_line('markers', 'requires_unix: Tests intended for Linux-based operating systems')
+    config.addinivalue_line('markers', 'requires_ci: Tests intended to only run in CI')
---
+++ postgres/tests/conftest.py
@@ -0,0 +1,125 @@
+# (C) Datadog, Inc. 2010-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+import copy
+import os
+
+import psycopg2
+import pytest
+from semver import VersionInfo
+
+from datadog_checks.dev import WaitFor, docker_run
+from datadog_checks.postgres import PostgreSql
+from datadog_checks.postgres.config import PostgresConfig
+from datadog_checks.postgres.metrics_cache import PostgresMetricsCache
+
+from .common import (
+    DB_NAME,
+    HOST,
+    PASSWORD,
+    PORT,
+    PORT_REPLICA,
+    PORT_REPLICA2,
+    PORT_REPLICA_LOGICAL,
+    POSTGRES_IMAGE,
+    POSTGRES_LOCALE,
+    POSTGRES_VERSION,
+    USER,
+)
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+INSTANCE = {
+    'host': HOST,
+    'port': PORT,
+    'username': USER,
+    'password': PASSWORD,
+    'dbname': DB_NAME,
+    'tags': ['foo:bar'],
+    'disable_generic_tags': True,
+}
+
+
+def connect_to_pg():
+    psycopg2.connect(host=HOST, dbname=DB_NAME, user=USER, password=PASSWORD)
+    if float(POSTGRES_VERSION) >= 10.0:
+        psycopg2.connect(host=HOST, dbname=DB_NAME, user=USER, port=PORT_REPLICA, password=PASSWORD)
+        psycopg2.connect(host=HOST, dbname=DB_NAME, user=USER, port=PORT_REPLICA2, password=PASSWORD)
+        psycopg2.connect(host=HOST, dbname=DB_NAME, user=USER, port=PORT_REPLICA_LOGICAL, password=PASSWORD)
+
+
+@pytest.fixture(scope='session')
+def dd_environment(e2e_instance):
+    """
+    Start a standalone postgres server requiring authentication.
+    """
+    compose_file = 'docker-compose.yaml'
+    if float(POSTGRES_VERSION) >= 10.0:
+        compose_file = 'docker-compose-replication.yaml'
+    with docker_run(
+        os.path.join(HERE, 'compose', compose_file),
+        conditions=[WaitFor(connect_to_pg)],
+        env_vars={"POSTGRES_IMAGE": POSTGRES_IMAGE, "POSTGRES_LOCALE": POSTGRES_LOCALE},
+    ):
+        yield e2e_instance
+
+
+@pytest.fixture
+def check():
+    c = PostgreSql('postgres', {}, [{'dbname': 'dbname', 'host': 'localhost', 'port': '5432', 'username': USER}])
+    c._version = VersionInfo(9, 2, 0)
+    return c
+
+
+@pytest.fixture
+def integration_check():
+    def _check(instance, init_config=None):
+        c = PostgreSql('postgres', init_config or {}, [instance])
+        return c
+
+    return _check
+
+
+@pytest.fixture
+def pg_instance():
+    return copy.deepcopy(INSTANCE)
+
+
+@pytest.fixture
+def pg_replica_instance():
+    instance = copy.deepcopy(INSTANCE)
+    instance['port'] = PORT_REPLICA
+    return instance
+
+
+@pytest.fixture
+def pg_replica_instance2():
+    instance = copy.deepcopy(INSTANCE)
+    instance['port'] = PORT_REPLICA2
+    return instance
+
+
+@pytest.fixture
+def pg_replica_logical():
+    instance = copy.deepcopy(INSTANCE)
+    instance['port'] = PORT_REPLICA_LOGICAL
+    return instance
+
+
+@pytest.fixture
+def metrics_cache(pg_instance):
+    config = PostgresConfig(instance=pg_instance, init_config={}, check={'warning': print})
+    return PostgresMetricsCache(config)
+
+
+@pytest.fixture
+def metrics_cache_replica(pg_replica_instance):
+    config = PostgresConfig(instance=pg_replica_instance, init_config={}, check={'warning': print})
+    return PostgresMetricsCache(config)
+
+
+@pytest.fixture(scope='session')
+def e2e_instance():
+    instance = copy.deepcopy(INSTANCE)
+    instance['dbm'] = True
+    instance['collect_resources'] = {'collection_interval': 0.1}
+    return instance
