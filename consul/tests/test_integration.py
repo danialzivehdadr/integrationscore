965e8bc65f69fc4614860288cb83eddc3be4af77
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-22 00:34:03 +0330

    Initialize 7.25.x

---
+++ consul/tests/test_integration.py
@@ -0,0 +1,137 @@
+# (C) Datadog, Inc. 2018-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import logging
+
+import pytest
+from requests import HTTPError
+
+from datadog_checks.consul import ConsulCheck
+
+from . import common
+
+METRICS = [
+    'consul.catalog.nodes_up',
+    'consul.catalog.nodes_passing',
+    'consul.catalog.nodes_warning',
+    'consul.catalog.nodes_critical',
+    'consul.catalog.services_up',
+    'consul.catalog.services_passing',
+    'consul.catalog.services_warning',
+    'consul.catalog.services_critical',
+    'consul.catalog.services_count',
+    'consul.catalog.total_nodes',
+    # Enable again when it's figured out why only followers submit these
+    # 'consul.net.node.latency.p95',
+    # 'consul.net.node.latency.min',
+    # 'consul.net.node.latency.p25',
+    # 'consul.net.node.latency.median',
+    # 'consul.net.node.latency.max',
+    # 'consul.net.node.latency.p99',
+    # 'consul.net.node.latency.p90',
+    # 'consul.net.node.latency.p75'
+]
+
+
+@pytest.mark.integration
+def test_check(aggregator, instance, dd_environment):
+    """
+    Testing Consul Integration
+    """
+    consul_check = ConsulCheck(common.CHECK_NAME, {}, [instance])
+    consul_check.check(instance)
+
+    for m in METRICS:
+        aggregator.assert_metric(m, at_least=0)
+
+    aggregator.assert_metric('consul.peers', value=3)
+
+    aggregator.assert_service_check('consul.check')
+    aggregator.assert_service_check('consul.up', tags=['consul_datacenter:dc1', 'consul_url:{}'.format(common.URL)])
+
+
+@pytest.mark.integration
+def test_single_node_install(aggregator, instance_single_node_install, dd_environment):
+    consul_check = ConsulCheck(common.CHECK_NAME, {}, [instance_single_node_install])
+    consul_check.check(instance_single_node_install)
+
+    for m in METRICS:
+        aggregator.assert_metric(m, at_least=1)
+
+    aggregator.assert_metric('consul.peers', value=3)
+
+    aggregator.assert_service_check('consul.check')
+    aggregator.assert_service_check('consul.up', tags=['consul_datacenter:dc1', 'consul_url:{}'.format(common.URL)])
+
+
+@pytest.mark.integration
+def test_acl_forbidden(instance_bad_token, dd_environment):
+    """
+    Testing Consul Integration with wrong ACL token
+    """
+    consul_check = ConsulCheck(common.CHECK_NAME, {}, [instance_bad_token])
+
+    got_error_403 = False
+    try:
+        consul_check.check(None)
+    except HTTPError as e:
+        if e.response.status_code == 403:
+            got_error_403 = True
+
+    assert got_error_403
+
+
+@pytest.mark.integration
+def test_prometheus_endpoint(aggregator, dd_environment, instance_prometheus, caplog):
+    consul_check = ConsulCheck(common.CHECK_NAME, {}, [instance_prometheus])
+    common_tags = instance_prometheus['tags']
+
+    if common.PROMETHEUS_ENDPOINT_AVAILABLE:
+        consul_check.check(instance_prometheus)
+
+        aggregator.assert_service_check(
+            'consul.prometheus.health',
+            tags=common_tags + ['endpoint:{}/v1/agent/metrics?format=prometheus'.format(common.URL)],
+        )
+
+        for metric in common.PROMETHEUS_METRICS:
+            aggregator.assert_metric(metric, tags=common_tags, count=1)
+
+        aggregator.assert_metric('consul.peers', value=3, count=1)
+        aggregator.assert_metric_has_tag_prefix('consul.raft.replication.appendEntries.logs', 'peer_id', count=2)
+
+        for hist_suffix in ['count', 'sum', 'quantile']:
+            aggregator.assert_metric_has_tag('consul.http.request.{}'.format(hist_suffix), 'method:GET', at_least=0)
+            for metric in common.PROMETHEUS_HIST_METRICS:
+                for tag in common_tags:
+                    aggregator.assert_metric_has_tag(metric + hist_suffix, tag, at_least=1)
+
+        aggregator.assert_all_metrics_covered()
+
+    else:
+        caplog.at_level(logging.WARNING)
+        consul_check.check(instance_prometheus)
+
+        assert (
+            "does not support the prometheus endpoint. "
+            "Update Consul or set back `use_prometheus_endpoint` to false to remove this warning." in caplog.text
+        )
+
+
+@pytest.mark.integration
+def test_version_metadata(aggregator, instance, dd_environment, datadog_agent):
+    consul_check = ConsulCheck(common.CHECK_NAME, {}, [instance])
+    consul_check.check_id = 'test:123'
+    consul_check.check(instance)
+
+    raw_version = common.CONSUL_VERSION.lstrip('v')  # some versions contain `v` prefix
+    major, minor, patch = raw_version.split('.')
+    version_metadata = {
+        'version.scheme': 'semver',
+        'version.major': major,
+        'version.minor': minor,
+        'version.patch': patch,
+        'version.raw': raw_version,
+    }
+
+    datadog_agent.assert_metadata('test:123', version_metadata)
---
+++ docs/developer/.scripts/33_render_status.py
@@ -0,0 +1,254 @@
+import os
+
+from datadog_checks.dev.tooling.utils import (
+    get_available_logs_integrations,
+    get_check_file,
+    get_config_file,
+    get_default_config_spec,
+    get_readme_file,
+    get_valid_checks,
+    get_valid_integrations,
+    has_logs,
+    has_agent_8_check_signature,
+    has_dashboard,
+    has_e2e,
+    has_process_signature,
+    has_saved_views,
+    is_tile_only,
+)
+
+MARKER = '<docs-insert-status>'
+
+
+def patch(lines):
+    """This renders the status of various aspects of integrations."""
+    if not lines or not (lines[0] == '# Status' and MARKER in lines):
+        return
+
+    marker_index = lines.index(MARKER)
+    new_lines = lines[:marker_index]
+
+    for renderer in (
+        render_config_spec_progress,
+        render_dashboard_progress,
+        render_metadata_progress,
+        render_logs_progress,
+        render_e2e_progress,
+        render_process_signatures_progress,
+        render_check_signatures_progress,
+        render_saved_views_progress,
+    ):
+        new_lines.extend(renderer())
+        new_lines.append('')
+
+    # Remove unnecessary final newline
+    new_lines.pop()
+
+    new_lines.extend(lines[marker_index + 1:])
+    return new_lines
+
+
+def render_config_spec_progress():
+    valid_checks = [x for x in sorted(get_valid_checks()) if not is_tile_only(x)]
+    total_checks = len(valid_checks)
+    checks_with_config_spec = 0
+
+    lines = ['## Config specs', '', None, '', '??? check "Completed"']
+
+    for check in valid_checks:
+        config_spec_path = get_default_config_spec(check)
+        if os.path.isfile(config_spec_path):
+            checks_with_config_spec += 1
+            status = 'X'
+        else:
+            status = ' '
+
+        lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_config_spec / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_dashboard_progress():
+    # Integrations that either do not emit metrics or have a too customer-specific setup to have an OOTBD
+    not_possible = {
+        'agent_metrics',  # Not for the end user
+        'snmp',  # Too custom
+        'openmetrics', # No default metrics
+        'pdh_check',  # No default metrics
+        'prometheus', # No default metrics
+        'teamcity',  # No metrics
+        'windows_service',  # No metrics
+        'win32_event_log',  # No metrics
+        'wmi_check',  # No default metrics
+        'windows_service'  # No metrics
+    }
+    valid_integrations = sorted(set(get_valid_integrations()).difference(not_possible))
+    total_integrations = len(valid_integrations)
+    integrations_with_dashboard = 0
+
+    lines = [
+        '## Dashboards',
+        '',
+        '!!! note',
+        '    This is not representative of _all_ dashboards, as many exist in legacy locations.',
+        '',
+        None,
+        '',
+        '??? check "Completed"',
+    ]
+
+    for integration in valid_integrations:
+        if has_dashboard(integration):
+            integrations_with_dashboard += 1
+            status = 'X'
+        else:
+            status = ' '
+
+        lines.append(f'    - [{status}] {integration}')
+
+    percent = integrations_with_dashboard / total_integrations * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[5] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_metadata_progress():
+    valid_checks = [x for x in sorted(get_valid_checks()) if not is_tile_only(x)]
+    total_checks = len(valid_checks)
+    checks_with_metadata = 0
+
+    lines = ['## Metadata submission', '', None, '', '??? check "Completed"']
+
+    for check in valid_checks:
+        status = ' '
+        check_file = get_check_file(check)
+        if os.path.exists(check_file):
+            with open(check_file, 'r', encoding='utf-8') as f:
+                contents = f.read()
+                if 'self.set_metadata' in contents:
+                    status = 'X'
+                    checks_with_metadata += 1
+        lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_metadata / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_logs_progress():
+    valid_checks = get_available_logs_integrations()
+    total_checks = len(valid_checks)
+    checks_with_logs = 0
+
+    lines = ['## Logs support', '', None, '', '??? check "Completed"']
+
+    for check in valid_checks:
+        status = None
+        tile_only = is_tile_only(check)
+        check_has_logs = has_logs(check)
+
+        if not tile_only:
+            status = ' '
+        if check_has_logs:
+            status = 'X'
+            checks_with_logs += 1
+
+        if status != 'X' and tile_only:
+            total_checks -= 1  # we cannot really add log collection to tile only integrations
+
+        if status is not None:
+            lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_logs / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_e2e_progress():
+    valid_checks = [x for x in sorted(get_valid_checks()) if not is_tile_only(x)]
+    total_checks = len(valid_checks)
+    checks_with_e2e = 0
+
+    lines = ['## E2E tests', '', None, '', '??? check "Completed"']
+
+    for check in valid_checks:
+        if has_e2e(check):
+            status = 'X'
+            checks_with_e2e += 1
+        else:
+            status = ' '
+        lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_e2e / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_process_signatures_progress():
+    exclude = {'datadog_checks_base', 'datadog_checks_dev', 'datadog_checks_downloader', 'snowflake'}
+    valid_checks = sorted([c for c in get_valid_checks() if c not in exclude])
+    total_checks = len(valid_checks)
+    checks_with_ps = 0
+
+    lines = ['## Process signatures', '', None, '', '??? check "Completed"']
+    for check in valid_checks:
+        if has_process_signature(check):
+            status = 'X'
+            checks_with_ps += 1
+        else:
+            status = ' '
+        lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_ps / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_check_signatures_progress():
+    exclude = {'datadog_checks_base', 'datadog_checks_dev', 'datadog_checks_downloader'}
+    valid_checks = sorted([c for c in get_valid_checks() if c not in exclude])
+    total_checks = len(valid_checks)
+    checks_with_cs = 0
+
+    lines = ['## Agent 8 check signatures', '', None, '', '??? check "Completed"']
+    for check in valid_checks:
+        if has_agent_8_check_signature(check):
+            status = 'X'
+            checks_with_cs += 1
+        else:
+            status = ' '
+        lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_cs / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
+
+
+def render_saved_views_progress():
+    valid_checks = get_available_logs_integrations()
+    total_checks = len(valid_checks)
+    checks_with_sv = 0
+
+    lines = ['## Default saved views', '', None, '', '??? check "Completed"']
+
+    for check in valid_checks:
+        if has_saved_views(check):
+            checks_with_sv += 1
+            status = 'X'
+        else:
+            status = ' '
+
+        lines.append(f'    - [{status}] {check}')
+
+    percent = checks_with_sv / total_checks * 100
+    formatted_percent = f'{percent:.2f}'
+    lines[2] = f'[={formatted_percent}% "{formatted_percent}%"]'
+    return lines
