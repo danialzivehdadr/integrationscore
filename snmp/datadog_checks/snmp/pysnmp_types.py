8c83e94074c1fece48da4c184c449bd97865b33f
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-22 00:22:19 +0000

    Initialize sangeeta.shivajirao/dbm-setup-actionable-errors

---
+++ snmp/datadog_checks/snmp/pysnmp_types.py
@@ -0,0 +1,58 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+"""
+Re-export PyASN1/PySNMP types and classes that we use, so that we can access them from a single module.
+"""
+
+import pysnmp.hlapi.v3arch.asyncio as hlapi
+from pyasn1.type.base import Asn1Type
+from pyasn1.type.univ import OctetString
+from pysnmp.hlapi.transport import AbstractTransportTarget
+from pysnmp.hlapi.v3arch.asyncio import (
+    CommunityData,
+    ContextData,
+    ObjectIdentity,
+    ObjectType,
+    SnmpEngine,
+    UdpTransportTarget,
+    UsmUserData,
+    usmDESPrivProtocol,
+    usmHMACMD5AuthProtocol,
+)
+from pysnmp.hlapi.v3arch.asyncio.lcd import CommandGeneratorLcdConfigurator
+from pysnmp.proto.rfc1902 import ObjectName, Opaque
+from pysnmp.proto.rfc3412 import MsgAndPduDispatcher
+from pysnmp.smi.builder import DirMibSource, MibBuilder
+from pysnmp.smi.exval import endOfMibView, noSuchInstance, noSuchObject
+from pysnmp.smi.instrum import MibInstrumController
+from pysnmp.smi.view import MibViewController
+
+lcd = CommandGeneratorLcdConfigurator()
+
+__all__ = [
+    'AbstractTransportTarget',
+    'Asn1Type',
+    'DirMibSource',
+    'CommunityData',
+    'ContextData',
+    'endOfMibView',
+    'hlapi',
+    'lcd',
+    'MibBuilder',
+    'MibInstrumController',
+    'MibViewController',
+    'MsgAndPduDispatcher',
+    'noSuchInstance',
+    'noSuchObject',
+    'ObjectIdentity',
+    'ObjectName',
+    'ObjectType',
+    'OctetString',
+    'Opaque',
+    'SnmpEngine',
+    'UdpTransportTarget',
+    'usmDESPrivProtocol',
+    'usmHMACMD5AuthProtocol',
+    'UsmUserData',
+]
